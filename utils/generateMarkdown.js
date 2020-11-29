660b4f1f95beab0d26c65f5ef877d1304854d21d
Nikolas Haug <nikohaug21@gmail.com>
2020-11-29 15:23:55 -0600

    add generate markdown functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,83 @@
+// Helper functions for the generateMarkdown function
+function renderLicenseBadge(license) {
+    if(license !== 'None') {
+        return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    }
+    return '';
+}
+
+function renderLicenseLink(license) {
+    if(license !== 'None') {
+        return (
+            `\n* [License](#license)\n`
+        )
+    }
+    return '';
+}
+
+function renderLicesnceSection(license) {
+    if(license !== 'None') {
+        return (
+            `## License
+            
+            This project is licensed under the ${license} license.
+            `
+        )
+    }
+    return '';
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
+    ${renderLicenseBadge(data.license)}
+
+    ## Description
+
+    ${data.description}
+
+    ## Table of Contents
+
+    * [Installation](#installation)
+
+    * [Usage](#usage)
+    ${renderLicenseLink(data.license)}
+    * [Contributing](#contributing)
+
+    * [Tests](#tests)
+
+    * [Questions](#questions)
+
+    ## Installation
+
+    To install necessary dependencies, run the following command:
+
+    \`\`\`
+    ${data.installation}
+    \`\`\`
+
+    ## Usage
+
+    ${data.usage}
+
+    ${renderLicesnceSection(data.license)}
+
+    ## Contributing
+
+    ${data.contributing}
+
+    ## Tests
+
+    To run tests, run the following command:
+
+    \`\`\`
+    ${data.test}
+    \`\`\`
+
+    ## Questions
+
+    If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my at [${data.github}](https://github.com/${data.github}/).
+
   `;
 }
 
